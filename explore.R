0a1a150f951a6be7b2fbdd05d28909796f71fa97
Thomas Levine <_@thomaslevine.com>
2015-05-13 08:05:00 +0000

    import

--- explore.R
+++ explore.R
@@ -1,1 +1,6 @@
-space <- read.csv('space.csv', colClasses = c(character, 'Date', logical)
+#!/usr/bin/env Rscript
+
+# Load spreadsheet
+space <- read.csv('space.csv', colClasses = c('character', 'numeric', 'logical'))
+space$timestamp <- as.POSIXct(space$timestamp, origin = '1970-01-01')
+names(space)[2] <- 'datetime'
