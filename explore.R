6be27a8f7928d58016ce3ffba5d920f3871566d6
Thomas Levine <_@thomaslevine.com>
2015-05-13 09:51:36 +0000

    -

--- explore.R
+++ explore.R
@@ -7,13 +7,12 @@ names(readings)[2] <- 'datetime'
 
 # Stuff
 library(dplyr)
-spaces <- group_by(readings, hackerspace)
-summarize(spaces, open = mean(open))
+space.days <- group_by(readings, hackerspace, day = weekdays(datetime),
+                       hour = as.numeric(strftime(datetime, '%H')))
+space.days <- select(space.days, hackerspace, day, hour, open)
+historical <- summarize(space.days, open = mean(open), count = n())
 
-space.days <- group_by(readings, hackerspace, day = weekdays(datetime))
-space.days <- select(space.days, hackerspace, day, open)
-summarize(space.days, open = mean(open))
-
+# Combine historical with whether the space was open in the previous hour?
 
 # 
 set.seed(284488333)
