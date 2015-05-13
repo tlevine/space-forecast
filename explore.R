e4ecb345809e2e7d1cf3804010ba0a7a231b2220
Thomas Levine <_@thomaslevine.com>
2015-05-13 08:10:22 +0000

    -

--- explore.R
+++ explore.R
@@ -1,6 +1,11 @@
 #!/usr/bin/env Rscript
 
 # Load spreadsheet
-space <- read.csv('space.csv', colClasses = c('character', 'numeric', 'logical'))
-space$timestamp <- as.POSIXct(space$timestamp, origin = '1970-01-01')
-names(space)[2] <- 'datetime'
+readings <- read.csv('space.csv', colClasses = c('character', 'numeric', 'logical'))
+readings$timestamp <- as.POSIXct(readings$timestamp, origin = '1970-01-01')
+names(readings)[2] <- 'datetime'
+
+# Stuff
+library(dplyr)
+spaces <- group_by(readings, hackerspace)
+summarize(spaces, open = mean(open))
