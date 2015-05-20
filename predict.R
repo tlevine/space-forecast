211c547b070fa4dc15e024cb29d817681dff7607
Thomas Levine <_@thomaslevine.com>
2015-05-20 21:05:59 +0000

    -

--- predict.R
+++ predict.R
@@ -1,12 +1,25 @@
 #!/usr/bin/env Rscript
 
-# Load spreadsheet
+# Load spreadsheet.
 readings <- read.csv('space.csv', colClasses = c('factor', 'numeric', 'factor'),
                      na.strings = NULL)
 readings$timestamp <- as.POSIXct(readings$timestamp, origin = '1970-01-01')
 names(readings)[2] <- 'datetime'
 
-# Subset to last week
+# Bucket by hour.
+readings$day <- factor(weekdays(readings$datetime))
+readings$hour <- as.numeric(strftime(readings$datetime, '%H'))
+
+# Subset to last week.
 end <- max(readings$datetime)
 start <- end - as.difftime(1, units = 'weeks')
 readings.lastweek <- subset(readings, datetime > start)
+
+# Come up with scores
+library(dplyr)
+
+features <- readings.lastweek %>%
+    group_by(space, day, hour) %>%
+    summarize(yes = sum(open == 'TRUE'),
+              no = sum(open == 'FALSE'),
+              broken = sum(open == 'NA'))
